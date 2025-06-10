408dc56c1b18bf1402689c48dd56bf1b5610c971
Nived Geethak <nived.geethak@nestgroup.net>
2025-06-10 15:26:36 +0530

    Second Commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/posts", (req, res) => {
   res.json(posts);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
